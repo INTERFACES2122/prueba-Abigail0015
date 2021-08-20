7d6d56c7461c27a698fe7d9d49ecc4bc3c8af398
Abigail0015 <A298360@alumnos.uaslp.mx>
2021-08-20 17:36:18 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion de Consola/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion_de_Consola
+{
+    class Alumno
+    {
+        int clave;
+        String nombre;
+        String apellidos;
+        Alumno(int clave, String nombre, String apellidos) 
+        {
+            this.clave = clave;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+    }
+}
